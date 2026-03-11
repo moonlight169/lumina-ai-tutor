bb84539a52ebd655665764ed1062bd25c487f0cd
Dzoixy <sittichaisc1@gmail.com>
2026-03-11 19:02:38 +0700

    LLM test

--- src/main.py
+++ src/main.py
@@ -1,34 +1,33 @@
 import asyncio
 from tts_engine import TTSEngine
-from stt_engine import stt
+from llm_handler import LLMEngine
 
-# สร้าง Instance ของ TTS ไว้ข้างนอก
 tts = TTSEngine()
+llm = LLMEngine()
 
 async def main():
-    print("--- Lumina AI System พร้อมใช้งาน ---")
-    print("(พูดคำว่า 'exit' เพื่อออกจากโปรแกรม)")
+
+    print("--- Lumina AI พร้อมใช้งาน ---")
+    print("(พิมพ์ exit เพื่อออก)")
 
     while True:
-        # ใช้ await ภายใน async function ได้แล้ว
-        text = await stt()
+
+        text = input("You: ")
 
         if not text:
             continue
-
-        print(f"คุณพูดว่า: {text}")
 
         if "exit" in text.lower():
             print("ปิดโปรแกรม...")
             break
 
-        # เรียกใช้ TTS (เช็กใน tts_engine ว่า speak เป็น async หรือเปล่า)
-        # ถ้าเพื่อนเขียนเป็น async ให้ใส่ await tts.speak(text)
-        tts.speak(text)
+        answer = await llm.generate(text)
+
+        print("Lumina:", answer)
+
+        # ไม่ต้อง await
+        tts.speak(answer)
+
 
 if __name__ == "__main__":
-    try:
-        # ใช้ asyncio.run เป็นตัวจุดระเบิดการทำงาน
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        pass  
+    asyncio.run(main())
